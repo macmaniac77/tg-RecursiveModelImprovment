a2b0ca38a8a511c085b9cafd3bf199b775411d0d
macmaniac77 <5971477+macmaniac77@users.noreply.github.com>
2026-09-18 17:15:32 -0400

    Add tg-aoi scaffold

---
+++ src/tgaoi/__init__.py
@@ -0,0 +1,5 @@
+from .ir import Graph, Node, TensorType
+from .serialize import dumps, loads, save, load
+
+__version__ = "0.1.0"
+__all__ = ["Graph", "Node", "TensorType", "dumps", "loads", "save", "load"]
