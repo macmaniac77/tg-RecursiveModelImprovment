bc5582674eb21811d8f037808936970bbebb339c
macmaniac77 <5971477+macmaniac77@users.noreply.github.com>
2026-09-19 12:50:31 -0400

    Export RF-DETR-derived AOIs

--- src/tgaoi/aoi/__init__.py
+++ src/tgaoi/aoi/__init__.py
@@ -1,8 +1,40 @@
 from .math import matmul_graph, softmax_graph
 from .nn import rmsnorm_graph, linear_graph, attention_graph
 from .optim import adam_graph
+from .vision import (
+    activation_graph,
+    layernorm_graph,
+    conv2d_graph,
+    patch_embedding_graph,
+    vit_self_attention_graph,
+    vit_mlp_graph,
+    vit_block_graph,
+    convx_graph,
+    bottleneck_graph,
+    c2f_graph,
+    multiscale_projector_graph,
+    dino_backbone_graph,
+)
+from .detection import (
+    bilinear_sampler_graph,
+    ms_deform_attention_graph,
+    detection_mlp_graph,
+    detr_decoder_layer_graph,
+    detr_decoder_graph,
+    hybrid_encoder_graph,
+    rfdetr_graph,
+)
+from .training import box_iou_graph, hungarian_matcher_graph, detr_set_criterion_graph
 
 __all__ = [
     "matmul_graph", "softmax_graph", "rmsnorm_graph", "linear_graph",
     "attention_graph", "adam_graph",
+    "activation_graph", "layernorm_graph", "conv2d_graph",
+    "patch_embedding_graph", "vit_self_attention_graph", "vit_mlp_graph",
+    "vit_block_graph", "convx_graph", "bottleneck_graph", "c2f_graph",
+    "multiscale_projector_graph", "dino_backbone_graph",
+    "bilinear_sampler_graph", "ms_deform_attention_graph", "detection_mlp_graph",
+    "detr_decoder_layer_graph", "detr_decoder_graph", "hybrid_encoder_graph",
+    "rfdetr_graph", "box_iou_graph", "hungarian_matcher_graph",
+    "detr_set_criterion_graph",
 ]
